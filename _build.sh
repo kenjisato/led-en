4ab0868e43124df7113c6328e97a1d12d009be88
Kenji Sato <mail@kenjisato.jp>
2017-04-07 20:22:21 +0900

    Start translation.

--- _build.sh
+++ _build.sh
@@ -2,6 +2,6 @@
 
 Rscript -e "bookdown::clean_book(clean = TRUE)"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', output_format = 'bookdown::pdf_book')"
+#Rscript -e "bookdown::render_book('index.Rmd', output_format = 'bookdown::pdf_book')"
 
-cp static/CNAME _book/CNAME
+#cp static/CNAME _book/CNAME
